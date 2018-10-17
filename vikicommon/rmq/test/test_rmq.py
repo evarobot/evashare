4d7af16335a561970981ddcdf562a850524110fa
chi.yuan <chi.yuan@bitmain.com>
2018-10-17 01:51:46 +0000

    modift rmq test

--- vikicommon/rmq/test/test_rmq.py
+++ vikicommon/rmq/test/test_rmq.py
@@ -1,48 +1,49 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
 import time
 
 from multiprocessing import Process
 from vikicommon.rmq import Receiver, Publisher
 
-
-class TestRMQ(object):
-    amqp_url = 'amqp://chi1st.xyz:5672?connection_attempts=3&heartbeat_interval=3600'
-    exchange_type = 'x-delayed-message'
-    exchange = 'delay_test'
-    arguments = {"x-delayed-type": "fanout"}
-    delay_time = 5000
-    binding_key = 't'
-
-    def consumer_callback(self, unused_channel, basic_deliver, properties,
-                          body):
-        send_time = time.strptime(body, '%Y-%m-%d %H:%M:%S')
-        receive_time = time.localtime()
-        assert receive_time.tm_sec - send_time.tm_sec == self.delay_time / 1000
-
-    def send_message(self):
-        publisher = Publisher(amqp_url=self.amqp_url, exchange=self.exchange,
-                              exchange_type=self.exchange_type,
-                              arguments=self.arguments)
-        date = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-        publisher.publish_message(date, self.binding_key,
-                                  delay_time=self.delay_time)
-        publisher.stop()
-
-    def receive_message(self):
-        receiver = Receiver(self.consumer_callback, self.amqp_url,
-                            self.exchange,
-                            binding_keys=[self.binding_key],
-                            queue_durable=True, queue_exclusive=False)
-        receiver.run()
-
-    @classmethod
-    def test_send_message(self):
-        p1 = Process(target=self.send_message)
-        p1.start()
-        p2 = Process(target=self.receive_message)
-        p2.start()
-        time.sleep(8)
-        p2.terminate()
-        p2.join()
+amqp_url = 'amqp://chi1st.xyz:5672?connection_attempts=3&heartbeat_interval=3600'
+exchange_type = 'x-delayed-message'
+exchange = 'delay_test'
+arguments = {"x-delayed-type": "fanout"}
+delay_time = 5000
+binding_key = 't'
 
 
-TestRMQ.test_send_message()
+def consumer_callback(unused_channel, basic_deliver, properties,
+                      body):
+    send_time = time.strptime(body, '%Y-%m-%d %H:%M:%S')
+    receive_time = time.localtime()
+    assert receive_time.tm_sec - send_time.tm_sec == delay_time / 1000
+
+
+def send_message():
+    publisher = Publisher(amqp_url=amqp_url, exchange=exchange,
+                          exchange_type=exchange_type,
+                          arguments=arguments)
+    date = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+    publisher.publish_message(date, binding_key,
+                              delay_time=delay_time)
+    publisher.stop()
+
+
+def receive_message():
+    receiver = Receiver(consumer_callback, amqp_url,
+                        exchange,
+                        binding_keys=[binding_key],
+                        queue_durable=True, queue_exclusive=False)
+    receiver.run()
+
+
+if __name__ == '__main__':
+    p1 = Process(target=send_message)
+    p1.start()
+    p2 = Process(target=receive_message)
+    p2.start()
+    time.sleep(8)
+    p2.terminate()
+    p2.join()
